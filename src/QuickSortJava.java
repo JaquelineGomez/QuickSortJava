6a8bf4dcb3af2ccbe5f0dafa7081cf9f18b78003
Jaqueline Gomez <91078781@mvusd.net>
2020-10-22 11:21:42 -0700

    Quicksort in java algorithm added

---
+++ src/QuickSortJava.java
@@ -0,0 +1,48 @@
+
+public class QuickSortJava
+{
+        public static void main(String[] args)
+        {
+            int[] arr={1,6,9,3,23};
+            QuickSortJava test1= new QuickSortJava();
+            test1.sort(arr,0, (arr.length-1));
+            for(int i=0; i< arr.length;i++)
+            {
+                System.out.print(arr[i]+"\t");
+            }
+        }
+
+    public int partition(int arr[], int first, int last)
+    {
+            int pivot = arr[last];
+            int i = (first - 1);
+            for (int j = first; j < last; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int temp = arr[i+1];
+            arr[i+1] = arr[last];
+            arr[last] = temp;
+
+            return i+1;
+        }
+
+        public void sort(int arr[], int first, int last)
+    {
+            if(first<last)
+            {
+                int p = partition(arr,first,last);
+
+                sort(arr, first,p-1);
+                sort(arr, p+1,last);
+            }
+    }
+
+}
